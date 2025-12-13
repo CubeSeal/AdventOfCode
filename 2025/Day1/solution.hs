d974281eb9660e2c78dfcac791e90d71303dba10
CubeSeal <kenndesilva1@gmail.com>
2025-12-13 11:24:53 +1100

    Day1 - Part 2

--- 2025/Day1/solution.hs
+++ 2025/Day1/solution.hs
@@ -3,11 +3,15 @@ module Main where
 main :: IO ()
 main = do
     instructions <- lines <$> readFile "input"
-    let result = calcZeroes instructions
-    print result
+    let result1 = part1 instructions
+        result2 = part2 instructions
+    putStrLn "Part 1 results"
+    print result1
+    putStrLn "Part 2 results"
+    print result2
 
-calcZeroes :: [String] -> (Int, Int)
-calcZeroes = foldl' go (50, 0) 
+part1 :: [String] -> (Int, Int)
+part1 = foldl' go (50, 0)
     where
         go :: (Int, Int) -> String -> (Int, Int)
         go (p, c) ('R':xs)
@@ -19,5 +23,21 @@ calcZeroes = foldl' go (50, 0)
             | pL' `mod` 100 == 0 = (pL', c + 1)
             | otherwise = (pL', c)
             where
+                pL' = p - read xs
+        go _ _ = undefined
+
+part2 :: [String] -> (Int, Int)
+part2 = foldl' go (50, 0)
+    where
+        go :: (Int, Int) -> String -> (Int, Int)
+        go (p, c) ('R':xs) = (pR', c + binPR' - binP)
+            where
+                binP = p `div` 100
+                binPR' = pR' `div` 100
+                pR' = p + read xs
+        go (p, c) ('L':xs) = (pL', c + binP - binPL')
+            where
+                binP = (p - 1) `div` 100
+                binPL' = (pL' - 1) `div` 100
                 pL' = p - read xs
         go _ _ = undefined
